aa1a5bbdd76abc8578f59188a382b7b1c7e80bcf
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-02-01 21:27:10 -0500

    finally got my current day forecast to work! A simple debugging is all it took.

--- code.js
+++ code.js
@@ -8,7 +8,6 @@ $("#search-button").on("click", function (event) {
         city +
         "&apikey=941abf696ec63ce79f180b076b8c17c5";
 
-    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -28,39 +27,44 @@ $("#search-button").on("click", function (event) {
         console.log("Your cities selections: " + hList);
         cityList(hList);
 
+
+
+        //Things we need now:
+        //1) a function that will take in our array of city names and loop through it to create the history section of our HTML
+        function cityList(hList) {
+            $("#cityDump").empty();
+            for (var i = 0; i < hList.length; i++) {
+                var cities = $("<button>");
+                cities.addClass("list");
+                cities.attr("data-name", hList[i]);
+                cities.text(hList[i]);
+                $("#cityDump").append(cities);
+            }
+        }
+
+        //2) a function that will use our response from openweathermap to create the "current weather" section of our HTML
+        function showWeather(response) {
+            debugger;
+            console.log("please work" + response);
+
+                var title = $("<h2>").addClass("card-title").text(response.name);
+                var card = $("<div>").addClass("card");
+                var wind = $("<p>").addClass("card-text").text("Wind speed is: " + response.wind.speed + " MPH");
+                var humidity = $("<p>").addClass("card-text").text("The humidity is: " + response.main.humidity + " %");
+                var temp = $("<p>").addClass("card-text").text("The temperature is: " + response.main.temp);
+                var cardBody = $("<div>").addClass("card-body");
+                var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+
+                title.append(image);
+                cardBody.append(title, wind, humidity, temp);
+                card.append(cardBody);
+                $("#current").append(card);
+    }
     });
 });
 
-//Things we need now:
-//1) a function that will take in our array of city names and loop through it to create the history section of our HTML
-function cityList(hList) {
-    $("#cityDump").empty();
-    for (var i = 0; i < hList.length; i++) {
-        var cities = $("<button>");
-        cities.addClass("list");
-        cities.attr("data-name", hList[i]);
-        cities.text(hList[i]);
-        $("#cityDump").append(cities);
-    }
-}
 
-//2) a function that will use our response from openweathermap to create the "current weather" section of our HTML
-function showWeather(response) {
-    console.log("is this working" + response);
 
-    //     var title = $("<h3>").addclass("card-title").text(data.name);
-    //     var card = $("<div>").addClass("card");
-    //     var wind = $("<p>").addClass("card-text").text("Wind speed is: " + data.wind.speed + " MPH");
-    //     var humidity = $("<p>").addClass("card-text").text("The humidity is: " + data.main.humidity + " %");
-    //     var temp = $("<p>").addClass("card-text").text("The temperature is: " + data.main.temp);
-    //     var cardBody = $("<div>").addClass("card-body");
-    //     var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-
-    //     title.append(image);
-    //     cardBody.append(title, wind, humidity, temp);
-    //     card.append(cardBody);
-    //     $("#current").append(card);
-}
 //3) check the "one call API" to see if it contains UVI info. If so, we can use that for both UVI and 5 day forecast
 //4) another ajax call (this can also be in a function) to get the 5 day forecast info from a different endpoint
 //5) IF the "one call API " does not return UVI info, then we will need yet another ajax call to get that information
